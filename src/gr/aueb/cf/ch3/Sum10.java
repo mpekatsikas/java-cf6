c13e5c5f3cc845781fdf25a8f235d4227518ba0a
mpekatsikas <mpekatsikas@gmail.com>
2024-05-26 13:51:52 +0300

    Chapter 3 commit

---
+++ src/gr/aueb/cf/ch3/Sum10.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.ch3;
+
+/**
+ * Υπολογίζει το 1+2+3+...+10 με επαναληπτικό
+ * τρόπο, με while..do.
+ *
+ * @author christosefthymiou
+ */
+public class Sum10 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+
+        while (i <= 10) {
+            sum = sum + i;  // sum += i;
+            i++;
+        }
+
+        System.out.println(sum);
+    }
+}
