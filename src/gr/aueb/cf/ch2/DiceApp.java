7342d67e14b1cf555bc535be8ac348f3bd7015a8
mpekatsikas <mpekatsikas@gmail.com>
2024-05-26 13:15:27 +0300

    Chapter 2 commits

---
+++ src/gr/aueb/cf/ch2/DiceApp.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch2;
+
+/**
+ * Dice Demo with Math.random
+ *
+ * @author christosefthymiou
+ *
+ */
+public class DiceApp {
+
+    public static void main(String[] args) {
+        int die1 = (int) (Math.random() * 6) + 1;
+        int die2 = (int) (Math.random() * 6) + 1;
+
+        System.out.printf("(%d, %d)", die1, die2);
+    }
+}
---
+++ src/gr/aueb/cf/ch2/OverflowApp.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.ch2;
+
+/**
+ * Ελέγχει την υπερχείληση στην περίπτωση
+ * που συμβεί.
+ *
+ * @author christosefthymiou
+ */
+public class OverflowApp {
+
+    public static void main(String[] args) {
+
+        // Δήλωση και αρχικοποίηση μεταβλητών
+        int num1 = 2_147_483_647;
+        int num2 = 2;
+        int result = 0;
+
+        // Εντολές
+        result = num1 + num2;
+
+        // Εκτύπωση αποτελέσματος
+        System.out.println("Το αποτέλεσμα είναι: " + result);
+    }
+}
